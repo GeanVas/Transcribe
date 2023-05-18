58e1f96ea6586bfc5b97591247d27608b8e29d23
GeanVas <gean.vasconez@gmail.com>
2023-05-18 01:11:22 -0500

    Diferente estilo

--- app/src/main/java/com/vasgan/speech_to_text_geeks/MainActivity.kt
+++ app/src/main/java/com/vasgan/speech_to_text_geeks/MainActivity.kt
@@ -1,70 +1,142 @@
 package com.vasgan.speech_to_text_geeks
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
-import android.widget.ImageView
+import android.speech.SpeechRecognizer
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
-import java.lang.Exception
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import java.util.*
-import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var outputTV: TextView
-    lateinit var  micIV: ImageView
-
-    private val REQUEST_CODE_SPEECH_INPUT = 1
+class MainActivity : AppCompatActivity(), RecognitionListener {
+    private val permission = 100
+    private lateinit var returnedText: TextView
+    private lateinit var toggleButton: ToggleButton
+    private lateinit var progressBar: ProgressBar
+    private lateinit var speech: SpeechRecognizer
+    private lateinit var recognizerIntent: Intent
+    private var logTag = "VoiceRecognitionActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        outputTV = findViewById(R.id.idTVOutput)
-        micIV = findViewById(R.id.idIVMic)
+        title = "TalkBoard"
+        returnedText = findViewById(R.id.textView)
+        progressBar = findViewById(R.id.progressBar)
+        toggleButton = findViewById(R.id.toggleButton)
+        progressBar.visibility = View.VISIBLE
 
-        micIV.setOnClickListener {
-            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speech = SpeechRecognizer.createSpeechRecognizer(this)
+        Log.i(logTag, "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(this))
+        speech.setRecognitionListener(this)
 
-            intent.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-            )
+        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, Locale.getDefault())
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
 
-            intent.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE,
-                Locale.getDefault()
-            )
-
-            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Escuchando...")
-
-            try {
-                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
-            } catch (e: Exception) {
-                Toast
-                    .makeText(
-                        this@MainActivity, " " + e.message,
-                        Toast.LENGTH_SHORT
-                    )
-                    .show()
+        toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+//                returnedText.text = "Listo para escuchar"
+                progressBar.visibility = View.VISIBLE
+                progressBar.isIndeterminate = true
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    permission)
+            } else {
+                progressBar.isIndeterminate = false
+                progressBar.visibility = View.VISIBLE
+                speech.stopListening()
             }
         }
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
-            if (resultCode == RESULT_OK && data != null) {
-                val res: ArrayList<String> =
-                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
-
-                outputTV.setText(
-                    Objects.requireNonNull(res[0])
-                )
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            permission -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager
+                    .PERMISSION_GRANTED) {
+                speech.startListening(recognizerIntent)
+                Log.i(logTag, "listening")
+            } else {
+                Toast.makeText(this@MainActivity, "Permission Denied!",
+                    Toast.LENGTH_SHORT).show()
             }
         }
+    }
+    override fun onStop() {
+        super.onStop()
+        speech.destroy()
+        Log.i(logTag, "destroy")
+    }
+    override fun onReadyForSpeech(params: Bundle?) {
+        returnedText.text = "Listo para escuchar"
+    }
+    override fun onRmsChanged(rmsdB: Float) {
+        progressBar.progress = rmsdB.toInt()
+    }
+    override fun onBufferReceived(buffer: ByteArray?) {
+        Log.i(logTag, "onBuffer")
+        Log.i(logTag, buffer.toString())
+    }
+    override fun onPartialResults(partialResults: Bundle?) {
+        Log.i(logTag, "onPartial")
+    }
+    override fun onEvent(eventType: Int, params: Bundle?) {
+        Log.i(logTag, "on event")
+    }
+    override fun onBeginningOfSpeech() {
+        Log.i(logTag, "onBeginningOfSpeech")
+        progressBar.isIndeterminate = false
+        progressBar.max = 10
+    }
+    override fun onEndOfSpeech() {
+        progressBar.isIndeterminate = true
+        toggleButton.isChecked = false
+    }
+    override fun onError(error: Int) {
+        val errorMessage: String = getErrorText(error)
+        Log.d(logTag, "FAILED $errorMessage")
+        returnedText.text = errorMessage
+        toggleButton.isChecked = false
+    }
+    private fun getErrorText(error: Int): String {
+        var message = ""
+        message = when (error) {
+            SpeechRecognizer.ERROR_AUDIO -> "Audio recording error"
+            SpeechRecognizer.ERROR_CLIENT -> "Client side error"
+            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "Insufficient permissions"
+            SpeechRecognizer.ERROR_NETWORK -> "Network error"
+            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "Network timeout"
+            SpeechRecognizer.ERROR_NO_MATCH -> "No match"
+            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RecognitionService busy"
+            SpeechRecognizer.ERROR_SERVER -> "error from server"
+            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "No speech input"
+            else -> "Didn't understand, please try again."
+        }
+        return message
+    }
+    override fun onResults(results: Bundle?) {
+        Log.i(logTag, "onResults")
+        val matches = results!!.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+        var text = ""
+        if (matches != null) {
+            for (result in matches) text = """
+          $result
+          """.trimIndent()
+        }
+        returnedText.text = text
     }
 }
